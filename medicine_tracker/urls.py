512d5b1d0396613ad2d05289cd4ef63e9c9bc9a7
ambroseotundo <ambroseotundo9@gmail.com>
2022-10-11 03:33:35 +0300

    set up files

--- medicine_tracker/urls.py
+++ medicine_tracker/urls.py
@@ -1,4 +1,4 @@
-"""Med_tracker URL Configuration
+"""medicine_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
